f724fe2c12c8f94fa8dd213d800708ef2838a5b9
catherineuhs <cliffordcat@urbandaleschools.com>
2019-10-07 14:23:02 -0500

    quiz

---
+++ testing.py
@@ -0,0 +1,15 @@
+#
+# for i in range(10, 0, -1):
+#     print("T minus", i)
+# print("blast off")
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i)
+#         print(j)
+
+total = 0
+for i in range(101):
+    num=int(input("please give me a number!"))
+    total+=num #total = total + num
+print("your total comes to:", total)
