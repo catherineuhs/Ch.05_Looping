3a729443423c3a3ef9369a544cc34a3b011bc102
catherineuhs <cliffordcat@urbandaleschools.com>
2019-10-08 14:23:18 -0500

    quiz

--- testing.py
+++ testing.py
@@ -8,8 +8,31 @@
 #         print(i)
 #         print(j)
 
-total = 0
-for i in range(101):
-    num=int(input("please give me a number!"))
-    total+=num #total = total + num
-print("your total comes to:", total)
+# total = 0
+# for i in range(101):
+#     num=int(input("please give me a number!"))
+#     total+=num #total = total + num
+# print("your total comes to:", total)
+#
+# i=0
+# while i<10
+#     print("Hi")
+#
+
+# total = 0
+# done = False
+# while not done:
+#     num = input("give number, q to quit")
+#     if num.lower()=="q"
+#         done = True
+#     else: num=int(num)
+#     total+=num
+# print("Your number is", total)
+
+
+import random
+num=random.randrange(10)
+print(num)
+
+#range(5,100)
+# ^^^ between 5 and 100
